47565da5ac37738cfa71494179ec9ffe3163d6b8
cherishegoyibo <cherishegoyibo@gmail.com>
2024-10-01 06:12:44 +0100

    mandatory-task

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,10 @@
-h 
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+    const newarray = [];
+    for (const item of array) {
+      newarray.push(appendString + item);
+    }
+  
+    return newarray;
+  }
