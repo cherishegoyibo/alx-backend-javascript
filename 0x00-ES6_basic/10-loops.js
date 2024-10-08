dbd62d5b4d91450b74d33c5db26fc1822c6cdd47
cherishegoyibo <cherishegoyibo@gmail.com>
2024-10-08 09:15:50 +0100

    req

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,10 @@
 /* eslint-disable guard-for-in */
 /* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-    const newarray = [];
-    for (const item of array) {
-      newarray.push(appendString + item);
-    }
-  
-    return newarray;
+  const newarray = [];
+  for (const item of array) {
+    newarray.push(appendString + item);
   }
+
+  return newarray;
+}
