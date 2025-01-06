a93c182adc931349f82a42cc7db386198a3edf95
cherishegoyibo <cherishegoyibo@gmail.com>
2025-01-06 13:38:47 +0100

    initial commit

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+function calculateNumber(a, b) {
+    const aRound = Math.round(a);
+    const bRound = Math.round(b);
+  
+    return aRound + bRound;
+  }
+  
+  module.exports = calculateNumber;
+  
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,2 @@
+# 0x06. Unittests in JS
+This directory contains task done for project 0x06. Unittests in JS
